81e2e7ee37c56167879cd6211798347bb89aa566
alexisvte <82931295+alexisvte@users.noreply.github.com>
2023-04-09 17:40:28 +0200

    Add files via upload

---
+++ app.R
@@ -0,0 +1,9 @@
+Sys.setenv("MAPBOX_ACCESS_TOKEN" = "pk.eyJ1IjoidXpoZWIiLCJhIjoiY2xmYmFub2V2MnlwcTN2cjByc3VpcWt4MCJ9.tPh8dV6S3boWr8u25dEKAA")
+
+#pdf(NULL)
+
+# run :
+
+# shinyApp("app")
+
+runApp("app")
